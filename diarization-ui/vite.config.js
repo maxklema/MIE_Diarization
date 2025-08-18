43aa98f97f1c7be1363386eb818745ed60177ca4
maxklema <maxklema@gmail.com>
2025-08-18 17:29:46 -0400

    testing automatic deployment

--- diarization-ui/vite.config.js
+++ diarization-ui/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
+  server: {
+    port: 5173,
+    host: "0.0.0.0",
+    allowedHosts: ["maxklema-MIE_Diarization-main.opensource.mieweb.org"],
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
